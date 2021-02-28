b1f9c8155921c642f4225e7e820528679cbc2192
YohanC04 <yohancartigny59@gmail.com>
2021-02-28 16:31:12 +0100

    Integration API

--- src/Controller/RechercheController.php
+++ src/Controller/RechercheController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+
+use Symfony\Component\HttpClient\HttpClient;
 use App\Form\Type\RechercheType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class RechercheController extends AbstractController{
 
@@ -30,7 +33,38 @@ class RechercheController extends AbstractController{
      */
 
     public function listFilm(){
-        return $this->render('Film/list.html.twig');
+
+        $httpClient = HttpClient::create();
+        $response = $httpClient->request(
+            'GET', 
+            'https://api.themoviedb.org/3/search/movie?api_key=b1a700d2ca02d17dc69cec6dd235dc6e&query=batman&language=fr'
+        );
+
+        
+
+        $statusCode = $response->getStatusCode();
+        /*echo $statusCode . "\n";*/
+
+        $contentType = $response->getHeaders()['content-type'][0];
+        /*echo $contentType . "\n";*/
+
+        $content = $response->getContent();
+        /*echo $content . "\n";*/
+
+
+        /* Convertir un objet en tableau */
+        $tableau = json_decode($content, true); //!!!!!!!
+        
+
+        /* Essai de manipulation tableau */
+        
+
+
+        
+
+
+        return $this->render('Film/list.html.twig' , ['content' => $tableau]);
+        
     }
         
 }
